5d681bbf0da3d044e9f57323b3a286f3bb3021f5
Alex Dunae <alex@dunae.ca>
2022-09-29 12:44:00 -0700

    Update test app for Rails 5.2

--- test/dummy/app/models/discussion.rb
+++ test/dummy/app/models/discussion.rb
@@ -1,5 +1,5 @@
 class Discussion < ActiveRecord::Base
-	belongs_to :discussable, :polymorphic => true
+	belongs_to :discussable, :polymorphic => true, :optional => true
 	
   acts_as_joinable_component :parent => 'discussable', :polymorphic => true
 end
--- test/dummy/db/migrate/20120604220913_initial_tables.rb
+++ test/dummy/db/migrate/20120604220913_initial_tables.rb
@@ -1,4 +1,4 @@
-class InitialTables < ActiveRecord::Migration
+class InitialTables < ActiveRecord::Migration[4.2]
   def change
 	  create_table :memberships, :force => true do |t|
 	    t.belongs_to :joinable, :polymorphic => true
